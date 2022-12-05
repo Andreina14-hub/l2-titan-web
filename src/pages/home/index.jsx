80edca7c29393260d9fe4108f89bbe584c34e55e
Andreina14-hub <84418323+Andreina14-hub@users.noreply.github.com>
2022-12-05 17:34:02 +0100

    img cambios

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -24,12 +24,14 @@ function HomePage() {
         <div className='content is-large'>
           <h3 className='title is-3 has-text-centered'>Title 3</h3>
         </div>
-        <div class='columns is-multiline is-align-content-center'>
-          <div class='column is-one-quarter-desktop is-half-tablet'>
+        <div className='columns is-multiline is-align-content-center'>
+          <div className='column is-one-quarter-desktop is-half-tablet'>
             <div className='card'>
               <div className='card-images'>
                 <figure className='image is-3by2'>
-        
+                  <img className='img' src='https://bulma.io/images/placeholders/256x256.png'>
+                    {' '}
+                  </img>
                 </figure>
               </div>
             </div>
@@ -38,7 +40,9 @@ function HomePage() {
             <div className='card'>
               <div className='card-images'>
                 <figure className='image is-3by2'>
-             
+                  <img className='img' src='https://bulma.io/images/placeholders/256x256.png'>
+                    {' '}
+                  </img>
                 </figure>
               </div>
             </div>
@@ -47,7 +51,9 @@ function HomePage() {
             <div className='card'>
               <div className='card-images'>
                 <figure className='image is-3by2'>
-              
+                  <img className='img' src='https://bulma.io/images/placeholders/256x256.png'>
+                    {' '}
+                  </img>
                 </figure>
               </div>
             </div>
